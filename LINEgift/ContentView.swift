72199b4910ee27f9c87d0418fec8867e3fc52dcb
caio <daisukecaio@icloud.com>
2026-03-24 11:44:25 +0900

    Initial Commit

---
+++ LINEgift/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LINEgift
+//
+//  Created by 新垣カイオ on 2026/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LINEgift/LINEgiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  LINEgiftApp.swift
+//  LINEgift
+//
+//  Created by 新垣カイオ on 2026/03/24.
+//
+
+import SwiftUI
+
+@main
+struct LINEgiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
